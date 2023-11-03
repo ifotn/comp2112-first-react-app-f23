4d526219d8561e32843a2089e4ae7329a79503f4
ifotn <rich@infrontofthenet.com>
2023-11-03 09:57:09 -0400

    l8/2 - blog w/effect & fetch

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,36 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Blog() {
-  const [data, setData] = useState([]);
+    // var to hold the api data
+    const [blogPosts, setBlogPosts] = useState([]);
 
-  useEffect(() => {
-    // This code runs when the component is mounted (equivalent to componentDidMount)
-    fetch('http://localhost:4000/api/posts')
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+    // when component "mounts" (aka loads), get external api data using Fetch
+    // the [] at the end tells React to only run this effect ONCE when component mounts
+    useEffect(() => {
+        fetch('https://blog-demo-d7iq.onrender.com/api/posts')
+        .then((response) => response.json())
+        .then((blogPosts) => setBlogPosts(blogPosts));
+        console.log(blogPosts);
+    },[]);
 
-  useEffect(() => {
-    // This code runs whenever the component re-renders (equivalent to componentDidUpdate)
-    console.log('Component updated');
-  });
-
-  return (
-    <div className="container">
-      <h1>Latest Posts</h1>
-      <ul className="list-group">
-        {data.map((post) => (
-          <li key={post.postId} className="list-group-item">
-            <h2><i className="bi bi-substack"></i> {post.title}</h2>
-            <div>
-                {(post.body).toString().substring(0,100)}...<a className="btn btn-info m-1 float-end" href={`/posts/${post.postId}`}><i className="bi bi-book"></i> Read More</a>
-            </div>
-            <div>Posted: <span className="badge text-bg-secondary">{(post.date)}</span></div>
-        </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Latest Random Thoughts</h1>
+            <ul className="list-group">
+                {blogPosts.map((post) => (
+                    <li className="list-group-item" key={post.postId}>
+                        <h2><i className="bi bi-substack"></i> {post.title}</h2>
+                        <div>
+                            {(post.body).toString().substring(0,100)} ... 
+                            <a href={`/post/${post.postId}`} className="btn btn-info float-end">
+                                <i className="bi bi-book"></i> Read More
+                            </a>
+                        </div>
+                        <div>
+                            Posted: 
+                                <span className="badge text-bg-secondary m-1">
+                                    {new Date(post.date).toLocaleDateString()}
+                                </span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Blog;
